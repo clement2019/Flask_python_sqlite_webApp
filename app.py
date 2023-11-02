1e6424396f3aa68e72b8a7917e2290a96d66bc19
clement ayeni <ayeniayokunle@yahoo.co.uk>
2023-11-02 09:06:05 +0100

    fresh files added

--- app.py
+++ app.py
@@ -1,14 +1,70 @@
-from flask import Flask, render_template, request
+from contextlib import redirect_stderr
+from flask import Flask, render_template, request,session
 import sqlite3 as sql
 app = Flask(__name__)
 
+#conn = sql.connect('database.db')
+#print("Opened database successfully")
+
+#conn.execute('CREATE TABLE students (name TEXT, addr TEXT, city TEXT, pin TEXT)')
+#print("Table created successfully")
+
+# conn.close()
+
 @app.route('/')
 def home():
-   return render_template('home.html')
+    name="Student management system"
+    return render_template('home.html',name = name)
 
 @app.route('/enternew')
 def new_student():
    return render_template('student.html')
+
+@app.route('/login')
+def login():
+   return render_template('login.html')
+
+
+
+#========
+
+@app.route('/log', methods =['GET', 'POST'])
+def log():
+    mesage = ''
+    if request.method == 'POST':
+       nm = request.form['nm']
+       pin = request.form['pin']
+       with sql.connect("database.db") as con:
+          cur = con.cursor()
+          #cur.execute("SELECT name,pin FROM students WHERE name = '" + name +"'AND pin = '" + pin + "'")
+          cur.execute("SELECT name,pin FROM students WHERE name = '"+nm+"' and pin='"+ pin+ "'")
+          user = cur.fetchall()
+      
+          if len(user) == 0:
+            #session['loggedin'] = True
+            #session['userid'] = user['userid']
+            #session['nm'] = user['nm']
+            #session['pin'] = user['pin']
+            mesage = 'Invalid credentails supplied,Please enter correct name / pin !'
+            return render_template('login.html', mesage = mesage)
+          else:
+            mesage = 'Logged in successfully !'
+            return render_template('userplatform.html', mesage = mesage)
+      
+           
+  
+@app.route('/logout')
+def logout():
+    session.pop('loggedin', None)
+    session.pop('nm', None)
+    session.pop('pin', None)
+    #return redirect_stderr(url_for('login'))
+    return render_template('login.html')
+
+
+
+
+
 
 @app.route('/addrec',methods = ['POST', 'GET'])
 def addrec():
@@ -22,11 +78,13 @@ def addrec():
          with sql.connect("database.db") as con:
             cur = con.cursor()
             
-            cur.execute("INSERT INTO students (name,addr,city,pin) 
-               VALUES (?,?,?,?)",(nm,addr,city,pin) )
+            
+            cur = con.cursor()
+            cur.execute("INSERT INTO students (name,addr,city,pin) VALUES (?,?,?,?)",(nm,addr,city,pin) )
             
             con.commit()
             msg = "Record successfully added"
+            
       except:
          con.rollback()
          msg = "error in insert operation"
